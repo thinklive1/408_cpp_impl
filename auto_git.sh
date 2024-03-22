8d0b8cf6dcb76823a59687f7bc6583be685a1ad4
thinklive1 <469631989@qq.com>
2024-03-22 13:09:50 +0800

    Fri Mar 22 13:09:50 CST 2024

--- auto_git.sh
+++ auto_git.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # 获取当前日期作为提交信息
-date=$(date +%F)
+Date=$(date)
 
 # 暂存当前目录所有文件
 git add . 
 
 # 提交暂存区文件
-git commit -m "$date"
+git commit -m "$Date"
 
 # 推送到远程仓库
-git push origin main
+git push
 
 echo "git commit and push succeed!"
