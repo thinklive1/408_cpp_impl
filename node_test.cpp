608535c92ff7465243be379ecd198818d5517fc3
thinklive1 <469631989@qq.com>
2024-03-25 14:35:18 +0800

    2024年 03月 25日 星期一 14:35:18 CST

--- node_test.cpp
+++ node_test.cpp
@@ -89,6 +89,49 @@ LinkList rev_insite_withhead(LinkList L) {
     return L;*/
 }
 
+/*6.插入排序带头结点的单链表L
+    1. 排序部分初始为头结点及其后续一个节点,未排序部分是head->next->next为首的链表
+    2. 取一个未排序节点，遍历找到插入位置
+    3. 插入节点，将leaft指向之前暂存的未排序部分首个结点
+*/
+void insertsort_list(LinkList L) {
+    LinkList leaft=L->next;//leaft是未排序部分的首个节点
+    LinkList remains=leaft->next;
+    leaft->next=NULL;
+    leaft=remains;
+    while(leaft!=NULL) {
+        remains=leaft->next;
+        LinkList prev = L;
+        while (prev->next != NULL && leaft->data > prev->next->data) prev = prev->next;
+        //注意要先判断next是否为空，否则会访问空指针出现异常！！！
+        leaft->next=prev->next;
+        prev->next=leaft;
+        leaft=remains;
+    }
+}
+
+//7. 删除无序带头结点单链表介于[x,y]内的值
+void del_val_inrange(LinkList L, int x, int y) {
+    LinkList p = L,q = L->next;
+    while (q!=NULL) {
+        if (q->data < y && q->data >x ) {
+            LinkList temp=q;
+            p->next = q->next;
+            delete temp;
+            q=p->next;
+            continue;
+        }
+        p=p->next;
+        q=q->next;
+    }
+}
+
+//8.找出两个链表的公共结点
+LinkList find_public_node(LinkList L1,LinkList L2) {
+    LinkList head = new LNode;
+    return head;
+}
+
 int main() {
     int temp[8] = { 2, 3, 17, 23, 28, 31, 42, 67 };
     LinkList nodes = new LNode;
@@ -98,6 +141,8 @@ int main() {
     // del_by_val_withhead(nodes,22);
     // print_linklist_rev(remove_head(nodes));
     // del_min_withhead(nodes);
-    nodes = rev_insite_withhead(nodes);
+    //nodes = rev_insite_withhead(nodes);
+    //insertsort_list(nodes);
+    //del_val_inrange(nodes,10,30);
     print_linklist(nodes);
 }
