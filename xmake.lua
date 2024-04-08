b615f213586394c0d802ff209a2d4fcd551487cd
thinklive1 <469631989@qq.com>
2024-04-08 14:21:41 +0800

    2024年 04月 08日 星期一 14:21:41 CST

--- xmake.lua
+++ xmake.lua
@@ -2,9 +2,13 @@ target("arr")
     set_kind("binary")
     add_files("./container/test/arr_test.cpp")
 
-target("node_list")
+target("node")
     set_kind("binary")
     add_files("./container/test/node_test.cpp")
+
+target("stack")
+    set_kind("binary")
+    add_files("./container/test/stack_and_queqe_test.cpp")
 
 target("tree")
     set_kind("binary")
