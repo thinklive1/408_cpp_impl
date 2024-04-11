2f39234c1a5b45574a1b84692acc50fdb07338a4
thinklive1 <469631989@qq.com>
2024-04-11 19:41:44 +0800

    Thu Apr 11 19:41:44 CST 2024

--- graph/Graph.cpp
+++ graph/Graph.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
 using namespace std;
-#define MaxSize 64
+#define MaxVertexNum 64
 #define ElemType int
+
+using VertexType = char;
+using EdgeType = int;
+struct Matrix_Graph {
+    VertexType Vex[MaxVertexNum];
+    EdgeType Edge[MaxVertexNum][MaxVertexNum]; //邻接矩阵，边表
+    int vexnum, arcnum; //图的当前顶点数和弧数
+};
+
+struct ArcNode { //边表结点
+    int adjvex; //该弧所指向的顶点的位置
+    struct ArcNode* next; //指向下一条弧的指针
+    //InfoType info; //网的边权值
+};
+
+struct VNode { //顶点表结点
+    VertexType data; //顶点信息
+    ArcNode* first; //指向第一条依附该项点的弧的指针
+};
+
+
+struct ALGraph {
+    VNode vertices[MaxVertexNum]; //邻接表
+    int vexnum, arcnum; //图的顶点数和弧数
+};
+
