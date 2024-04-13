5f0deec505e17d62a95a9a8f3a4ce85f1e394b30
thinklive1 <469631989@qq.com>
2024-04-13 16:28:24 +0800

    Sat Apr 13 16:28:24 CST 2024

--- auto_git.sh
+++ auto_git.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 Date=$(date)
 cd ~/408_cpp_impl || exit
+find . -depth -name "*.o" -type f -delete 
+find . -depth -name "*.exe" -type f -delete 
 git add . 
 git commit -m "$Date"
 git push
