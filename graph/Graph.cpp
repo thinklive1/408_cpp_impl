baa6d3627c6c05c6dbb7d4b7461c96dda6d2620a
thinklive1 <469631989@qq.com>
2024-04-13 23:21:34 +0800

    Sat Apr 13 23:21:34 CST 2024

--- graph/Graph.cpp
+++ graph/Graph.cpp
@@ -1,48 +1,54 @@
 #include <iostream>
 #include <queue>
+#include <set>
+#include <vector>
 using namespace std;
-#define MaxVertexNum 64
-#define ElemType int
+const int MaxVNodeNum = 64;;
+const int MaxVertexNum = 64;;
 
-using VertexType = char;
+using ElemType = int;
+using VNodeType = int;
 using EdgeType = int;
 
 struct Matrix_Graph {
-    VertexType Vex[MaxVertexNum];
-    EdgeType Edge[MaxVertexNum][MaxVertexNum]; //邻接矩阵，边表
+    VNodeType Vex[MaxVertexNum];
+    EdgeType Edge[MaxVNodeNum][MaxVertexNum]; //邻接矩阵，边表
     int vexnum, arcnum; //图的当前顶点数和弧数
 };
-
+struct VNode;
 struct ArcNode { //边表结点
-    int adjvex; //该弧所指向的顶点的位置
+    VNode* adjvex; //该弧所指向的顶点的指针
     struct ArcNode* next; //指向下一条弧的指针
     //InfoType info; //网的边权值
 };
 
 struct VNode { //顶点表结点
-    VertexType data; //顶点信息
+    VNodeType no; //顶点编号
     ArcNode* first; //指向第一条依附该项点的弧的指针
 };
 
 
 struct ALGraph {
-    VNode vertices[MaxVertexNum]; //邻接表
+    VNode vertices[MaxVNodeNum]; //邻接表
     int vexnum, arcnum; //图的顶点数和弧数
 };
+
+using GraphAdjList = ALGraph;
+using Vertex = VNode;
 
 ALGraph build_graph(int V[], pair<int, int> edges[], int V_size, int e_size) {
     ALGraph G;
     G.vexnum = V_size;G.arcnum = e_size;
     for (int i = 0;i < G.vexnum;i++) {
         VNode v;
-        v.data = V[i];
+        v.no = i;
         v.first = nullptr;
         G.vertices[i] = v;
     }
     for (int i = 0;i < G.arcnum;i++) {
         auto e = edges[i];
         ArcNode* arc = new ArcNode;
-        arc->adjvex = e.second;
+        arc->adjvex = &G.vertices[e.second];
         arc->next = nullptr;
         auto temp = G.vertices[e.first].first;
         G.vertices[e.first].first = arc;
@@ -55,8 +61,68 @@ void print_graph(ALGraph G) {
     for (int i = 0;i < G.vexnum;i++) {
         auto p = G.vertices[i].first;
         while (p) {
-            cout << i << "->" << p->adjvex << '\n';
+            cout << i << "->" << p->adjvex->no << '\n';
             p = p->next;
         }
     }
+}
+
+/* 广度优先遍历 */
+// 使用邻接表来表示图，以便获取指定顶点的所有邻接顶点
+vector<Vertex*> graphBFS(GraphAdjList& graph, Vertex* startVet) {
+    // 顶点遍历序列
+    vector<Vertex*> res;
+    // 哈希表，用于记录已被访问过的顶点
+    set<Vertex*> visited = { startVet };
+    // 队列用于实现 BFS
+    queue<VNode*> que;
+    que.push(startVet);
+    // 以顶点 vet 为起点，循环直至访问完所有顶点
+    while (!que.empty()) {
+        Vertex* vet = que.front();
+        que.pop();          // 队首顶点出队
+        res.push_back(vet); // 记录访问顶点
+        // 遍历该顶点的所有邻接顶点
+        auto adjVet = vet->first;
+        while (adjVet) {
+            if (visited.count(adjVet->adjvex)) {
+                adjVet = adjVet->next;
+                continue;            // 跳过已被访问的顶点
+            }
+            que.push(adjVet->adjvex);        // 只入队未访问的顶点
+            visited.emplace(adjVet->adjvex); // 标记该顶点已被访问
+            adjVet = adjVet->next;
+        }
+    }
+    // 返回顶点遍历序列
+    return res;
+}
+
+/* 深度优先遍历辅助函数 */
+void dfs(ALGraph& graph, set<VNode*>& visited, vector<VNode*>& res, VNode* vet) {
+    res.push_back(vet);   // 记录访问顶点
+    visited.emplace(vet); // 标记该顶点已被访问
+    // 遍历该顶点的所有邻接顶点
+    auto adjVet = vet->first;
+    vector<VNode*> temp = {};
+    while (adjVet) {
+        temp.push_back(adjVet->adjvex);
+        adjVet = adjVet->next;
+    }
+    for (auto V : temp) {
+        if (visited.count(V))
+            continue; // 跳过已被访问的顶点
+        dfs(graph, visited, res, V); // 递归访问邻接顶点
+    }
+}
+
+/* 深度优先遍历 */
+// 使用邻接表来表示图，以便获取指定顶点的所有邻接顶点
+vector<VNode*> graphDFS(ALGraph& graph, VNode* startVet) {
+    // 顶点遍历序列
+    vector<VNode*> res;
+    // 哈希表，用于记录已被访问过的顶点
+    set<VNode*> visited;
+    dfs(graph, visited, res, startVet);
+    return res;
 }
--- graph/test/Graph_test.cpp
+++ graph/test/Graph_test.cpp
@@ -12,18 +12,18 @@ int IsExistEL(MGraph G) {
     return 0;
 }
 
+//1.将邻接表方式表示的图G转换为邻接矩阵arcs,V是图的顶点数
 int* Convert(ALGraph& G, int V, int arcs[]) {
-    //将邻接表方式表示的图G转换为邻接矩阵arcs,V是图的顶点数
     //为了方便，二维矩阵在存储上视为一维，结点位置实际是i*V+j
     for (int i = 0;i < V;i++) for (int j = 0;j < V;j++) arcs[i * V + j] = 0;
     for (int i = 0; i < V; i++) { //依次遍历各顶点表结点为头的边链表
         ArcNode* p = G.vertices[i].first;
         while (p != nullptr) { //遍历边链表
-            if (p->adjvex >= V) { //需要注意ArcNode储存的顶点数不可以超过V，否则会数组越界
+            if (p->adjvex->no >= V) { //需要注意ArcNode储存的顶点数不可以超过V，否则会数组越界
                 cout << "数组越界\n";
                 return nullptr;
             }
-            arcs[i * V + p->adjvex] = 1;
+            arcs[i * V + p->adjvex->no] = 1;
             p = p->next;
         } //while
     }
@@ -33,7 +33,6 @@ int* Convert(ALGraph& G, int V, int arcs[]) {
     return arcs;
 }
 
-
 int main() {
     int V[5] = { 0,1,2,3,4 };
     pair<int, int> edges[6] = { {1,2},{1,3},{2,4},{4,3},{3,2},{4,2} };
@@ -41,4 +40,10 @@ int main() {
     print_graph(g);
     int matrix[25];
     Convert(g, 5, matrix);
+    //for (auto i : graphBFS(g, &g.vertices[1])) cout << i->no;
+    //for (auto i : graphDFS(g, &g.vertices[2])) cout << i->no;
+
+
+
+
 }
