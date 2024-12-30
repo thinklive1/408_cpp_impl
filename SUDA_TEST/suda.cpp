dab6864d4f4edaebfab773163d50c434f063a67c
thinklive1 <469631989@qq.com>
2024-12-30 23:11:07 +0800

    Mon Dec 30 23:11:07 CST 2024

--- SUDA_TEST/suda.cpp
+++ SUDA_TEST/suda.cpp
@@ -1,6 +1,25 @@
 #include <vector>
+#include <iostream>
+#include <sstream>
 using namespace std;
 
+//c++输入输出,当cin>>从缓冲区中读取数据时，若缓冲区中第一个字符是空格、tab或换行这些分隔符时，cin>>会将其忽略并清除，继续读取下一个字符，若缓冲区为空，则继续等待。但是如果读取成功，字符后面的分隔符是残留在缓冲区的，cin>>不做
+//1. 计算a+b
+void a_plus_b() {
+    cout << "please input two numbers : " << '\n';
+    int a, b;
+    while (cin >> a >> b) cout << "a+b is " << a + b << '\n';
+}
+
+void read_from_line() {
+    string l;
+    while (getline(cin, l)) {
+        stringstream ss(l);
+        int sum = 0, num;
+        while (ss >> num) sum += num;
+        cout << sum << '\n';
+    }
+}
 
 vector<int> twoSum(vector<int>& nums, int target) {
     vector<int> temp, result;
@@ -25,4 +44,8 @@ vector<int> twoSum(vector<int>& nums, int target) {
     }
     return result;
 };
+
+int main() {
+    //a_plus_b();
+}
 
