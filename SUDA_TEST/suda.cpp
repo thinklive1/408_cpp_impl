ebbe4a2b21e9616ad5fbd995f14791083ed14cd6
thinklive1 <469631989@qq.com>
2025-02-09 23:32:29 +0800

    Sun Feb  9 23:32:29 CST 2025

--- SUDA_TEST/suda.cpp
+++ SUDA_TEST/suda.cpp
@@ -11,6 +11,12 @@
 #define null INT16_MIN
 using namespace std;
 
+template<typename container>
+void prit_container(container c) {
+    if (c.empty()) return;
+    for (int i = 0;i < c.size();i++) cout << i << ": " << c[i] << '\n';
+}
+
 //c++输入输出,当cin>>从缓冲区中读取数据时，若缓冲区中第一个字符是空格、tab或换行这些分隔符时，cin>>会将其忽略并清除，继续读取下一个字符，若缓冲区为空，则继续等待。但是如果读取成功，字符后面的分隔符是残留在缓冲区的，cin>>不做
 //1. 计算a+b
 void a_plus_b() {
@@ -569,9 +575,201 @@ int maxArea(vector<int>& height) {
     return max_ar;
 }
 
+//找到零和不重复的3数
+/*需要保证：
+    第二重循环枚举到的元素不小于当前第一重循环枚举到的元素；
+    第三重循环枚举到的元素不小于当前第二重循环枚举到的元素。
+    将数组中的元素从小到大进行排序，然后固定一个元素随后用双指针逼近另外两个
+*/
+vector<vector<int>> threeSum(vector<int>& nums) {
+    if (nums.size() <= 2) return {};
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> res;
+    for (int i = 0;i < nums.size();i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        int k = nums.size() - 1;
+        for (int j = i + 1;j < nums.size();j++) {
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+            int t = nums[i] + nums[j];
+            while (k > j && t + nums[k] > 0) --k;
+            if (j == k) continue;
+            if (nums[i] + nums[j] + nums[k] == 0) res.push_back({ nums[i], nums[j], nums[k] });
+        }
+    }
+    return res;
+}
+
+int lengthOfLongestSubstring(string s) {
+    if (s.size() <= 1) return s.size();
+    unordered_set<char> hash;
+    size_t max_len = 1;
+    int i = 0, j = 1;
+    hash.insert(s[i]);
+    while (1) {
+        while (j < s.size() && !hash.count(s[j])) {
+            hash.insert(s[j]);
+            ++j;
+        }
+        max_len = max(max_len, hash.size());
+        if (j == s.size()) break;
+        else {
+            hash.erase(s[i]);
+            ++i;
+        }
+    }
+    return max_len;
+}
+
+vector<int> findAnagrams(string s, string p) {
+    vector<int> res;
+    unordered_map<char, int> hash_p;
+    for (char c : p) {
+        if (!hash_p.count(c)) hash_p[c] = 1;
+        else ++hash_p[c];
+    }
+    int i = 0, j = 0;
+    while (1) {
+        auto hash = hash_p;
+        int  j = i;
+        while (j < s.size() && !hash.empty()) {//检查以i开始的子串是否匹配
+            if (!hash.count(s[j])) break;
+            else {
+                --hash[s[j]];
+                if (hash[s[j]] == 0) hash.erase(s[j]);
+                ++j;
+            }
+        }
+        if (hash.empty()) res.emplace_back(i);//如果匹配添加索引
+        if (j >= s.size()) break;//如果越界退出
+        //不匹配需要移到下一个可能匹配的i
+        ++i;
+        while (!hash_p.count(s[i])) {
+            if (i > s.size() - p.size()) return res;
+            ++i;
+        }
+    }
+    return res;
+}
+
+vector<int> findAnagrams_with_window(string s, string p) {
+    int sLen = s.size(), pLen = p.size();
+
+    if (sLen < pLen) {
+        return vector<int>();
+    }
+
+    vector<int> ans;
+    vector<int> sCount(26);
+    vector<int> pCount(26);
+    for (int i = 0; i < pLen; ++i) {
+        ++sCount[s[i] - 'a'];
+        ++pCount[p[i] - 'a'];
+    }
+
+    if (sCount == pCount) {
+        ans.emplace_back(0);
+    }
+
+    for (int i = 0; i < sLen - pLen; ++i) {
+        --sCount[s[i] - 'a'];
+        ++sCount[s[i + pLen] - 'a'];
+
+        if (sCount == pCount) {
+            ans.emplace_back(i + 1);
+        }
+    }
+
+    return ans;
+}
+
+vector<int> findAnagrams_best(string s, string p) {//统计滑动窗口和字符串 p 中每种字母数量的差
+    int sLen = s.size(), pLen = p.size();
+    if (sLen < pLen) return {};
+    vector<int> ans;
+    vector<int> count(26);
+    for (int i = 0; i < pLen; ++i) {//计算第一个窗口的字符情况
+        ++count[s[i] - 'a'];
+        --count[p[i] - 'a'];
+    }
+    int differ = 0;//子串和p不同的字母数量
+    for (int j = 0; j < 26; ++j) if (count[j] != 0) ++differ;//不为0的元素说明有差异
+    if (differ == 0) ans.emplace_back(0);
+    for (int i = 0; i < sLen - pLen; ++i) {
+        --count[s[i] - 'a'];//i右移移除对应count，正式的+1由for循环完成,接下来处理differ变化
+        if (count[s[i] - 'a'] == 0) --differ;  // 如果i处是多余字符，即count的对应值为1，i右移后differ会减少
+        else if (count[s[i] - 'a'] == -1)  ++differ; // 如果i处正好相抵，i右移后differ会增加
+
+        ++count[s[i + pLen] - 'a'];
+        if (count[s[i + pLen] - 'a'] == 0)  --differ;
+        else if (count[s[i + pLen] - 'a'] == 1)  ++differ;
+
+        if (differ == 0) ans.emplace_back(i + 1);
+    }
+    return ans;
+}
+
+//mp记录从0开始的子序列和及对应序列数量，若[0-j]-[0-i]==k，则找到了mp对应数量的子序列
+int subarraySum(vector<int>& nums, int k) {
+    unordered_map<int, int> mp;
+    mp[0] = 1;
+    int count = 0, pre = 0;
+    for (auto& x : nums) {
+        pre += x;
+        if (mp.count(pre - k)) {
+            count += mp[pre - k];
+        }
+        mp[pre]++;
+    }
+    return count;
+}
+
+int maxSubArray(vector<int>& nums) {
+    unordered_map<int, int> mp;
+    if (nums.size() == 1) return nums[0];
+    int max_sum = nums[0], pre_sum = max_sum, min_sum = min(0, nums[0]);
+    for (int i = 1;i < nums.size();i++) {
+        min_sum = min(min_sum, pre_sum);
+        pre_sum += nums[i];
+        max_sum = max(max_sum, pre_sum - min_sum);
+    }
+    return max_sum;
+}
+
+/*
+线段树
+ struct Status {
+        int lSum, rSum, mSum, iSum;
+    };
+
+    Status pushUp(Status l, Status r) {
+        int iSum = l.iSum + r.iSum;
+        int lSum = max(l.lSum, l.iSum + r.lSum);
+        int rSum = max(r.rSum, r.iSum + l.rSum);
+        int mSum = max(max(l.mSum, r.mSum), l.rSum + r.lSum);
+        return (Status) {lSum, rSum, mSum, iSum};
+    };
+
+    Status get(vector<int> &a, int l, int r) {
+        if (l == r) {
+            return (Status) {a[l], a[l], a[l], a[l]};
+        }
+        int m = (l + r) >> 1;
+        Status lSub = get(a, l, m);
+        Status rSub = get(a, m + 1, r);
+        return pushUp(lSub, rSub);
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        return get(nums, 0, nums.size() - 1).mSum;
+    }
+
+*/
+
+
+
 int main() {
     //a_plus_b();
-    vector <int> test = { 0,3,7,2,5,8,4,6,0,1,2,1 };
+    vector <int> test = { -2,-1 };
     vector <int> test2 = { 1,3,4 };
     vector<string> strs = { "eat","tea","tan","ate","nat","bat" };
     ListNode temp(65535);
@@ -592,6 +790,12 @@ int main() {
     //cout << climbStairs(5);
     //groupAnagrams(strs);
     //cout << longestConsecutive(test);
+    //threeSum(test);
+    //cout << lengthOfLongestSubstring("abcabcbb");
+    //prit_container(findAnagrams("cbaebabacd", "abc"));
+    //prit_container(findAnagrams("ababababab", "aab"));
+    //cout << subarraySum(test, 2);
+    cout << maxSubArray(test);
 
     //strings_withdot();
     //auto si = twoSum2(test, 6);
